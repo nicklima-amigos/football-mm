de5fbd79b7610795e978e92d8593db8cf072fc48
Raphael Oliveira <raphael.foliveira1@gmail.com>
2023-08-16 20:42:38 -0300

    test: implement players tests

--- src/players/players.controller.spec.ts
+++ src/players/players.controller.spec.ts
@@ -1,29 +1,101 @@
+import { NestApplication } from '@nestjs/core';
 import { Test, TestingModule } from '@nestjs/testing';
 import { TypeOrmModule, getRepositoryToken } from '@nestjs/typeorm';
-import { RepositoryMock, getRepositoryMock } from '../../test/mocks/repository';
+import * as supertest from 'supertest';
+import { Repository } from 'typeorm';
+import { fakePlayer, fakePlayers } from '../../test/factories/players.factory';
 import { Player } from './entities/player.entity';
 import { PlayerController } from './players.controller';
 import { PlayerService } from './players.service';
+import { getRepositoryMock } from '../../test/mocks/repository';
+import { ValidationPipe } from '@nestjs/common';
 
 describe('PlayerController', () => {
+  let app: NestApplication;
   let controller: PlayerController;
-  let repository: RepositoryMock<Player>;
+  let repository: Repository<Player>;
 
   beforeEach(async () => {
-    repository = getRepositoryMock();
     const module: TestingModule = await Test.createTestingModule({
-      imports: [TypeOrmModule.forFeature([Player])],
       controllers: [PlayerController],
-      providers: [PlayerService],
-    })
-      .overrideProvider(getRepositoryToken(Player))
-      .useValue(repository)
-      .compile();
+      providers: [
+        PlayerService,
+        {
+          provide: getRepositoryToken(Player),
+          useValue: getRepositoryMock(),
+        },
+      ],
+    }).compile();
 
     controller = module.get<PlayerController>(PlayerController);
+    repository = module.get<Repository<Player>>(getRepositoryToken(Player));
+    app = module.createNestApplication();
+    app.useGlobalPipes(new ValidationPipe());
+    await app.init();
+  });
+
+  afterEach(async () => {
+    jest.resetAllMocks();
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('findAll', () => {
+    it('should return a list of players', async () => {
+      const players = fakePlayers(10);
+      jest.spyOn(repository, 'find').mockResolvedValueOnce(players);
+      const expected = JSON.parse(JSON.stringify(players));
+
+      const response = await supertest(app.getHttpServer()).get('/players');
+      const actual = response.body;
+
+      expect(response.status).toEqual(200);
+      expect(actual).toEqual(expected);
+    });
+  });
+
+  describe('findOne', () => {
+    it('should return a single player when given a valid id', async () => {
+      const player = fakePlayer();
+      player.id = 1;
+      jest.spyOn(repository, 'findOne').mockResolvedValueOnce(player);
+      const expected = JSON.parse(JSON.stringify(player));
+
+      const response = await supertest(app.getHttpServer()).get('/players/1');
+      const actual = response.body;
+
+      expect(response.status).toEqual(200);
+      expect(actual).toEqual(expected);
+    });
+  });
+
+  describe('create', () => {
+    it('should create a player', async () => {
+      const player = fakePlayer();
+      jest.spyOn(repository, 'create').mockReturnValueOnce(player);
+      const { id, ...playerInfo } = player;
+      const expected = JSON.parse(JSON.stringify(player));
+
+      const response = await supertest(app.getHttpServer())
+        .post('/players')
+        .send(playerInfo);
+      const actual = response.body;
+      console.log(actual);
+
+      expect(response.status).toEqual(201);
+      expect(actual).toEqual(expected);
+    });
+
+    it('should throw an error when given invalid data', async () => {
+      jest.spyOn(repository, 'create').mockReturnValue(fakePlayer());
+
+      const response = await supertest(app.getHttpServer())
+        .post('/players')
+        .send({ foo: 'invalid' });
+
+      expect(response.status).toEqual(400);
+    });
   });
 });
--- test/mocks/repository.ts
+++ test/mocks/repository.ts
@@ -4,14 +4,14 @@ import { DeleteResult, UpdateResult } from 'typeorm';
 export type RepositoryMock<T> = {
   find: jest.Mock<Promise<T[]>>;
   findOne: jest.Mock<Promise<T | undefined>>;
-  create: jest.Mock<Promise<T>>;
+  create: jest.Mock<T>;
   save: jest.Mock<Promise<T | T[]>>;
   delete: jest.Mock<Promise<DeleteResult>>;
   remove: jest.Mock<Promise<T>>;
   update: jest.Mock<Promise<UpdateResult>>;
 };
 
-export const getRepositoryMock = () => ({
+export const getRepositoryMock = (): RepositoryMock<any> => ({
   find: jest.fn(),
   findOne: jest.fn(),
   create: jest.fn(),
