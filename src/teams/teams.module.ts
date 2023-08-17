b2c4e0ab5c65145834a88c183e6bc799fd696413
Raphael Oliveira <raphael.foliveira1@gmail.com>
2023-08-16 23:03:35 -0300

    fix: add Player as entity in TypeOrmModule.forFeature

--- src/teams/teams.module.ts
+++ src/teams/teams.module.ts
@@ -4,9 +4,10 @@ import { DatabaseModule } from 'src/database/database.module';
 import { Team } from './entities/team.entity';
 import { TeamController } from './teams.controller';
 import { TeamService } from './teams.service';
+import { Player } from '../players/entities/player.entity';
 
 @Module({
-  imports: [DatabaseModule, TypeOrmModule.forFeature([Team])],
+  imports: [DatabaseModule, TypeOrmModule.forFeature([Team, Player])],
   controllers: [TeamController],
   providers: [TeamService],
 })
