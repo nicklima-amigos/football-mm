acb5cd07620873f65186a20ab5700f4c2b51447e
Nicolas B. Lima <100323924+nicolas-limadev@users.noreply.github.com>
2023-08-19 13:44:42 -0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,8 +11,3 @@ provider "azurerm" {
   features {}
   skip_provider_registration=true
 }
-
-resource "azurerm_resource_group" "football-mm" {
-  name     = "football-mm2"
-  location = "West US"
-}
