65023bd951590873635632d404f7b0c7bc52ba30
nicolas-limadev <nicolasbasto76@gmail.com>
2023-08-18 23:42:31 -0300

    fix-add-variaveis-tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,6 +9,11 @@ terraform {
 
 provider "azurerm" {
   features {}
+
+  client_id       = var.agent_client_id
+  client_secret   = var.agent_client_secret
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
 }
 
 resource "azurerm_resource_group" "football-mm" {
