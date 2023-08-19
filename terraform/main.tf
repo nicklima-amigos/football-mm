e495f7127f2d1e7709a328b54eb678ec0d2bfd62
nicolas-limadev <nicolasbasto76@gmail.com>
2023-08-18 21:56:30 -0300

    pipeline-terraform-main

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,17 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
 terraform {
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.70.0"
     }
   }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "football-mm" {
+  name = "football-mm"
+  location = "West US"
 }
