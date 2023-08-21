51da147495ec1af7ea28b2f5b57e90ac4ccb38c8
Raphael Oliveira <raphael.foliveira1@gmail.com>
2023-08-20 22:44:59 -0300

    feat: implemente league service

--- src/league/league.service.ts
+++ src/league/league.service.ts
@@ -1,26 +1,48 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateLeagueDto } from './dto/create-league.dto';
 import { UpdateLeagueDto } from './dto/update-league.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { League } from './entities/league.entity';
+import { In, Repository } from 'typeorm';
+import { Match } from '../match/entities/match.entity';
 
 @Injectable()
 export class LeagueService {
-  create(createLeagueDto: CreateLeagueDto) {
-    return 'This action adds a new league';
+  constructor(
+    @InjectRepository(League) private repository: Repository<League>,
+    @InjectRepository(Match) private matchRepository: Repository<Match>,
+  ) {}
+
+  async create(createLeagueDto: CreateLeagueDto) {
+    const { name, matchIds } = createLeagueDto;
+    const matches = await this.matchRepository.find({
+      where: { id: In(matchIds) },
+    });
+    return this.repository.save({
+      name,
+      matches,
+    });
   }
 
   findAll() {
-    return `This action returns all league`;
+    return this.matchRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} league`;
+  async findOne(id: number) {
+    const league = await this.repository.findOne({ where: { id } });
+    if (!league) {
+      throw new HttpException('League not found', HttpStatus.NOT_FOUND);
+    }
+    return league;
   }
 
-  update(id: number, updateLeagueDto: UpdateLeagueDto) {
-    return `This action updates a #${id} league`;
+  async update(id: number, updateLeagueDto: UpdateLeagueDto) {
+    await this.findOne(id);
+    return this.repository.update({ id }, updateLeagueDto);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} league`;
+  async remove(id: number) {
+    const league = await this.findOne(id);
+    return this.repository.remove(league);
   }
 }
