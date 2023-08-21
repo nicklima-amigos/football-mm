d44fb4250ead32fe0893da3030bc585de2b04a67
Nicolas B. Lima <100323924+nicolas-limadev@users.noreply.github.com>
2023-08-21 16:17:50 -0300

    update-test main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,35 +9,52 @@ terraform {
 
 provider "azurerm" {
   features {}
-
 }
 
-data "azurerm_resource_group" "football-mm" {
-  name = "football-mm"
+variable "resource_group_name" {
+  description = "Name of the Azure resource group"
+  type        = string
+  default     = "football-mm"
 }
 
-output "id" {
-  value = data.azurerm_resource_group.football-mm.id
+variable "vm_admin_username" {
+  description = "Admin username for the virtual machine"
+  type        = string
+  default     = "admin"
+}
+
+variable "vm_admin_password" {
+  description = "Admin password for the virtual machine"
+  type        = string
+  default     = "Admin"
+}
+
+resource "azurerm_resource_group" "football_mm" {
+  name = var.resource_group_name
+}
+
+data "azurerm_resource_group" "football_mm_data" {
+  name = azurerm_resource_group.football_mm.name
 }
 
 resource "azurerm_virtual_network" "main" {
   name                = "football-network"
   address_space       = ["10.0.0.0/16"]
-  location            = data.azurerm_resource_group.football-mm.location
-  resource_group_name = data.azurerm_resource_group.football-mm.name
+  location            = azurerm_resource_group.football_mm.location
+  resource_group_name = azurerm_resource_group.football_mm.name
 }
 
 resource "azurerm_subnet" "internal" {
   name                 = "internal"
-  resource_group_name  = data.azurerm_resource_group.football-mm.name
+  resource_group_name  = azurerm_resource_group.football_mm.name
   virtual_network_name = azurerm_virtual_network.main.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
 resource "azurerm_network_interface" "main" {
   name                = "football-nic"
-  location            = data.azurerm_resource_group.football-mm.location
-  resource_group_name = data.azurerm_resource_group.football-mm.name
+  location            = azurerm_resource_group.football_mm.location
+  resource_group_name = azurerm_resource_group.football_mm.name
 
   ip_configuration {
     name                          = "testconfiguration1"
@@ -48,16 +65,10 @@ resource "azurerm_network_interface" "main" {
 
 resource "azurerm_virtual_machine" "main" {
   name                  = "football-vm"
-  location              = data.azurerm_resource_group.football-mm.location
-  resource_group_name   = data.azurerm_resource_group.football-mm.name
+  location              = azurerm_resource_group.football_mm.location
+  resource_group_name   = azurerm_resource_group.football_mm.name
   network_interface_ids = [azurerm_network_interface.main.id]
   vm_size               = "Standard_DS1_v2"
-
-  # Uncomment this line to delete the OS disk automatically when deleting the VM
-  # delete_os_disk_on_termination = true
-
-  # Uncomment this line to delete the data disks automatically when deleting the VM
-  # delete_data_disks_on_termination = true
 
   storage_image_reference {
     publisher = "Canonical"
@@ -73,8 +84,8 @@ resource "azurerm_virtual_machine" "main" {
   }
   os_profile {
     computer_name  = "hostname"
-    admin_username = "admin"
-    admin_password = "Admin"
+    admin_username = var.vm_admin_username
+    admin_password = var.vm_admin_password
   }
   os_profile_linux_config {
     disable_password_authentication = false
@@ -84,3 +95,6 @@ resource "azurerm_virtual_machine" "main" {
   }
 }
 
+output "resource_group_id" {
+  value = azurerm_resource_group.football_mm.id
+}
