778747cb7ddc6c3315eafac450508b8dbb4dc344
Nicolas B. Lima <100323924+nicolas-limadev@users.noreply.github.com>
2023-08-21 13:41:20 -0300

    include network-nic main.tf (#45)

--- terraform/main.tf
+++ terraform/main.tf
@@ -24,6 +24,25 @@ resource "azurerm_virtual_network" "main" {
   resource_group_name = azurerm_resource_group.football-mm.name
 }
 
+resource "azurerm_subnet" "internal" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.football-mm.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "main" {
+  name                = "football-nic"
+  location            = azurerm_resource_group.football-mm.location
+  resource_group_name = azurerm_resource_group.football-mm.name
+
+  ip_configuration {
+    name                          = "testconfiguration1"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
 resource "azurerm_virtual_machine" "main" {
   name                  = "football-vm"
   location              = azurerm_resource_group.football-mm.location
