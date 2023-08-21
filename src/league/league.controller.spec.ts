191183c64d5b101a61c5c3f40f1f4b0bdc190328
Raphael Oliveira <raphael.foliveira1@gmail.com>
2023-08-20 22:48:49 -0300

    fix: use providers instead of importing typeormmodule

--- src/league/league.controller.spec.ts
+++ src/league/league.controller.spec.ts
@@ -1,19 +1,28 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { getRepositoryMock } from '../../test/mocks/repository';
+import { Match } from '../match/entities/match.entity';
+import { League } from './entities/league.entity';
 import { LeagueController } from './league.controller';
 import { LeagueService } from './league.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { League } from './entities/league.entity';
-import { Match } from '../match/entities/match.entity';
-import { Player } from '../players/entities/player.entity';
 
 describe('LeagueController', () => {
   let controller: LeagueController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [TypeOrmModule.forFeature([League, Match, Player])],
       controllers: [LeagueController],
-      providers: [LeagueService],
+      providers: [
+        LeagueService,
+        {
+          provide: getRepositoryToken(Match),
+          useValue: getRepositoryMock<Match>(),
+        },
+        {
+          provide: getRepositoryToken(League),
+          useValue: getRepositoryMock<League>(),
+        },
+      ],
     }).compile();
 
     controller = module.get<LeagueController>(LeagueController);
