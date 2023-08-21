341056c615000e319b8a514ebe99f2a51c115981
Raphael Oliveira <raphael.foliveira1@gmail.com>
2023-08-20 22:46:30 -0300

    chore: add typeormmodule import to league testing module

--- src/league/league.controller.spec.ts
+++ src/league/league.controller.spec.ts
@@ -1,12 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { LeagueController } from './league.controller';
 import { LeagueService } from './league.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { League } from './entities/league.entity';
+import { Match } from '../match/entities/match.entity';
+import { Player } from '../players/entities/player.entity';
 
 describe('LeagueController', () => {
   let controller: LeagueController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [TypeOrmModule.forFeature([League, Match, Player])],
       controllers: [LeagueController],
       providers: [LeagueService],
     }).compile();
