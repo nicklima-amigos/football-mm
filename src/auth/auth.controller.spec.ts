85a068f2f943b5b527e61f71338cb71f5a90a197
Raphael Oliveira <raphael.foliveira1@gmail.com>
2023-08-24 00:28:35 -0300

    test: add mock providers to new modules

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,6 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { getRepositoryMock } from '../../test/mocks/repository';
+import { Session } from './entities/session.entity';
 
 describe('AuthController', () => {
   let controller: AuthController;
@@ -8,7 +11,13 @@ describe('AuthController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        {
+          provide: getRepositoryToken(Session),
+          useValue: getRepositoryMock<Session>(),
+        },
+      ],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -1,6 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { getRepositoryMock } from '../../test/mocks/repository';
 
 describe('UserController', () => {
   let controller: UserController;
@@ -8,7 +11,13 @@ describe('UserController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
-      providers: [UserService],
+      providers: [
+        UserService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: getRepositoryMock<User>(),
+        },
+      ],
     }).compile();
 
     controller = module.get<UserController>(UserController);
