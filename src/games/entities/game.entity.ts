bf42149f394e541258c7f7e67a7db08a789e1058
Raphael Oliveira <raphael.foliveira1@gmail.com>
2023-08-27 18:21:43 -0300

    chore: remove unused imports

--- src/games/entities/game.entity.ts
+++ src/games/entities/game.entity.ts
@@ -1,4 +1,1 @@
-import { ChildEntity, JoinTable, ManyToMany } from 'typeorm';
-import { BaseGame } from '../../base-game/entities/base-game.entity';
-import { Player } from '../../players/entities/player.entity';
 
--- src/match/entities/match.entity.ts
+++ src/match/entities/match.entity.ts
@@ -1,6 +1,1 @@
-import { ChildEntity, Entity, ManyToOne, TableInheritance } from 'typeorm';
-import { BaseGame } from '../../base-game/entities/base-game.entity';
-import { League } from '../../league/entities/league.entity';
-import { Team } from '../../teams/entities/team.entity';
-
 
