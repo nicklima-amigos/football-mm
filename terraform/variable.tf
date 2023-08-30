60dd5d32658a8b41fb79b45d75c1d501793faec0
Nicolas B. Lima <100323924+nicolas-limadev@users.noreply.github.com>
2023-08-30 13:31:09 -0300

    Update variable.tf

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,3 +1,21 @@
+variable "location" {
+  description = "Variável que indica a região onde os recursos vão ser criados"
+  type        = string
+  default     = "West Europe"
+}
+
+variable "account_tier" {
+  description = "Tier da Storage Account na Azure"
+  type        = string
+  default     = "Standard"
+}
+
+variable "account_replication_type" {
+  description = "Tipo de replicação de dados da Storage Account"
+  type        = string
+  default     = "LRS"
+}
+
 variable "resource_group_name" {
   description = "Name of the Azure resource group"
   type        = string
