954aa74fa8151049b67562173f9d3be9b2f42d6a
Raphael Oliveira <raphael.foliveira1@gmail.com>
2023-09-17 11:49:37 -0300

    fix: remove unused import

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -1,9 +1,4 @@
-import {
-  CanActivate,
-  ExecutionContext,
-  Injectable,
-  UnauthorizedException,
-} from '@nestjs/common';
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { Request } from 'express';
 import { jwtConstants } from './constants';
