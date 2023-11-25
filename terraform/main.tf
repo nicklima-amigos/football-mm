8b7bb4237bec59bd1366acc5c1ea3ee0e4f5e036
nicolas-limadev <nicolasbasto76@gmail.com>
2023-11-25 19:54:40 -0300

    fix: commit das correcoes do TF e git actions

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,54 +27,9 @@ resource "azurerm_storage_account" "football_storage_account" {
 
 }
 
-resource "azurerm_virtual_network" "main" {
-  name                = "football-network"
-  address_space       = ["10.0.0.0/16"]
-  location            = var.location
-  resource_group_name = var.resource_group_name
-}
-
-resource "azurerm_subnet" "internal" {
-  name                 = "internal"
-  resource_group_name  = var.resource_group_name
-  virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes     = ["10.0.2.0/24"]
-}
-
-resource "azurerm_public_ip" "public_ip" {
-  name                = "mypublicip"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  allocation_method   = "Dynamic"
-}
-
-resource "azurerm_network_interface" "main" {
-  name                = "football-nic"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-
-  ip_configuration {
-    name                          = "testconfiguration1"
-    subnet_id                     = azurerm_subnet.internal.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.public_ip.id
-  }
-}
-
 data "azurerm_ssh_public_key" "ssh_pub_key" {
   name                = "azure-key-vm"
   resource_group_name = var.resource_group_name
-}
-
-resource "azurerm_image" "packer_image" {
-  name                = "PackerImageFootball"
-  resource_group_name = var.resource_group_name
-  location            = var.location
-
-  os_disk {
-    os_type  = "Linux"
-    blob_uri = ""
-  }
 }
 
 
@@ -90,9 +45,6 @@ resource "azurerm_virtual_machine" "main" {
     caching           = "ReadWrite"
     create_option     = "FromImage"
     managed_disk_type = "Standard_LRS"
-
-    # Referência à imagem criada pelo Packer
-    image_uri = azurerm_image.packer_image.id
   }
   os_profile {
     computer_name  = "hostname"
